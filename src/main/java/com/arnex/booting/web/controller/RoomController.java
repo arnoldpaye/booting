6d21d1154a638cdac185c08a590943ca14fb07dd
Arnold Paye <arnoldpayet@gmail.com>
2025-11-19 09:18:54 -0400

    add room service

--- src/main/java/com/arnex/booting/web/controller/RoomController.java
+++ src/main/java/com/arnex/booting/web/controller/RoomController.java
@@ -7,28 +7,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.arnex.booting.data.entity.RoomEntity;
-import com.arnex.booting.data.repository.RoomRepository;
-import com.arnex.booting.web.model.Room;
+import com.arnex.booting.service.RoomService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequestMapping("/rooms")
 public class RoomController {
-    private final RoomRepository roomRepository;
+    private final RoomService roomService;
 
-    public RoomController(RoomRepository roomRepository) {
-        this.roomRepository = roomRepository;
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
     }
 
     @GetMapping
     public String getRoomsPage(Model model) {
-        List<RoomEntity> roomEntities = this.roomRepository.findAll();
-        List<Room> rooms = new ArrayList<>(roomEntities.size());
-        roomEntities.forEach(e -> rooms.add(new Room(e.getRoomId(), e.getName(), e.getNumber(), e.getBedInfo())));
-        model.addAttribute("rooms", rooms);
+        model.addAttribute("rooms", this.roomService.getAllRooms());
         return "rooms";
     }
-    
 }
